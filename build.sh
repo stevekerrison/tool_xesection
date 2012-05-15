af4f7196a64113a924bd03007b4316f09dcb8867
Steve Kerrison <steve@stevekerrison.com>
2012-05-15 12:12:52 +0100

    Added main source and build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+g++ -O2 -Wall src/XE.cpp src/xesection.cpp -o bin/xesection
---
+++ src/xesection.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2012, Steve Kerrison, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
+#include "XE.h"
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+	return 0;
+}
